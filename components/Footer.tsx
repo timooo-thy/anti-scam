98094dfe685dc0f9e9e3f1441724e5064230359a
Timothy Lee <timothylhy@hotmail.com>
2024-03-03 00:38:58 +0800

    updated footer size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-background py-10 text-center text-sm md:text-lg">
+    <footer className="bg-background py-10 text-center text-sm md:text-base">
       <p>© {year} SG Anti-Scam AI. All rights reserved.</p>
     </footer>
   );
