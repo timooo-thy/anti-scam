68839fc6662cc487b91ea5b2394864ece4fc86e9
Timothy Lee <timothylhy@hotmail.com>
2024-04-10 22:43:50 +0800

    file change

--- src/components/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  const year = new Date().getFullYear();
-  return (
-    <footer className="bg-background py-10 text-center text-sm md:text-base">
-      <p>© {year} SG Anti-Scam AI. All rights reserved.</p>
-    </footer>
-  );
-}
