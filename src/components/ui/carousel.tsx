2999da93b771545246fff0edb3c8b19e77cf120e
Timothy Lee <timothylhy@hotmail.com>
2024-04-10 22:09:03 +0800

    file path fixes

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -6,8 +6,8 @@ import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react";
 
-import { cn } from "@/src/lib/utils";
-import { Button } from "@/src/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
